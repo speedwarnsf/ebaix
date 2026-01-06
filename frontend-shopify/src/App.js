6ac154fb7139581c366fc577d3527cae3a4b28d2
DustinY15 <DUSTINYORK15@GMAIL.COM>
2026-01-06 13:51:21 -0800

    Nudge header left and lift logo

--- frontend-shopify/src/App.js
+++ frontend-shopify/src/App.js
@@ -36,13 +36,13 @@ function App() {
         <div className="w-full flex justify-center pb-4" style={{ paddingTop: "142px" }}>
           <div
             className="w-full flex flex-col items-start gap-2"
-            style={{ maxWidth: "min(420px, calc(100vw - 48px))", marginLeft: "-12px" }}
+            style={{ maxWidth: "min(420px, calc(100vw - 48px))", marginLeft: "-15px" }}
           >
             <img
               src={`${process.env.PUBLIC_URL}/shopify/assets/NudioOverClear.png`}
               alt="nudio logotype"
               className="w-48 sm:w-64 h-auto"
-              style={{ marginLeft: "-19px", marginTop: "-130px" }}
+              style={{ marginLeft: "-19px", marginTop: "-142px" }}
               loading="lazy"
             />
             <p className="text-sm text-white/70" style={{ marginTop: "-95px" }}>
